f916f05f116306ac047a5ee8166015fcfc0c5aa5
Cheese-Space <batumete1011@gmail.com>
2026-04-17 14:21:02 +0200

    implement traits for Error type

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,48 @@
 extern crate alloc;
 #[cfg(not(feature = "std"))]
 use alloc::string::String;
+#[cfg(not(feature = "std"))]
+use alloc::boxed::Box;
+use alloc::string::ToString;
 #[cfg(feature = "std")]
 extern crate std;
 #[cfg(feature = "std")]
 use std::string::String;
+#[cfg(feature = "std")]
+use std::boxed::Box;
 #[cfg(not(feature = "std"))]
 use core::error::Error as StdError;
 #[cfg(feature = "std")]
 use std::error::Error as StdError;
-
+use core::fmt;
 pub struct Error {
     msg: String,
     context: Option<String>
 }
+impl From<Box<dyn StdError>> for Error {
+    fn from(value: Box<dyn StdError>) -> Self {
+        Self {
+            msg: value.to_string(),
+            context: None
+        }
+    }
+}
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(context) = &self.context {
+            write!(f, "{context}\n\nCaused by:\n\t{}", self.msg)
+        }
+        else {
+            write!(f, "{}", self.msg)
+        }
+    }
+}
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(&self, f)
+    }
+}
 pub type Result<T, E = Error> = core::result::Result<T, E>;
 pub trait Context<T> {
-    fn context(self) -> Result<T>;
+    fn context(self, context: &str) -> Result<T>;
 }
